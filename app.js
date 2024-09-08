26e5bf0d2b22e09101ea1d92e189086fca4ab24b
fazalshah52 <bilalfazal52@gmail.com>
2024-09-08 23:04:24 +0500

    firt assignment in javascript

---
+++ app.js
@@ -0,0 +1,216 @@
+        // alert(15254254)
+        // document.write(145622)
+        // console.log(1255456)
+
+        // var firstname = "fazal"
+        // var lastname = "shah"
+        // var city = "karachi"
+        // document.write("my name is " + firstname + " " + lastname + " i live in " + city)
+
+        // var num = 50;
+        
+        // console.log(typeof num)
+
+
+
+
+
+/**********************  Chapter No 1 ALERT ***************************************/
+
+        
+
+
+
+
+        // Qno_1  Write a script to greet your website visitor using JS alert
+        // box
+
+        alert("Your Welcome");
+
+
+        // Qno 2. Write a script to display following message on your web
+        // page
+
+        alert("Error! Please Enter a valid password");
+
+        // Qno 3. Write a script to display following message on your web
+        // page: (Hint : Use line break
+
+        alert("Welcome to JS Land... \n Happy Coding!");
+
+        // Qno 4. Write a script to display following messages in sequence:
+
+        alert("Happy Coding! \nPrevent this page from creating additional dialogs.");
+
+        // Qno 5. Generate the following message through browser’s
+        // developer console:
+        
+        alert("Hello... i can run JS through my web browser's console");
+
+
+
+
+// **************  Chapter No 2 VARIABLES   *********************
+
+
+
+
+
+// Qno 1. Declare a variable called username.
+
+var username;
+
+// Qno 2. Declare a variable called myName & assign to it a string 
+
+var myName = "fazal Shah";
+
+// Qno 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var message;
+message = "Hello World";
+alert(message);
+
+// Qno 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var studentName = "Jhone Doe";
+var studentAge = " 15 years old";
+var studentGrade = "Certified Mobile Application Development"
+alert(studentName);
+alert(studentAge);
+alert(studentGrade);
+
+
+// Qno 5. Write a script to display the following alert using one JS
+// variable:
+
+alert(" PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Qno 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+
+var email = "example@example.com"
+alert("Hello, my email address is " + email);
+
+// Qno 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+document.write("Yah! i can write HTML content through JavaScript");
+
+
+// Qno 9. Store following string in a variable and show in alert and
+// browser through JS
+
+var styles = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(styles);
+
+
+
+
+
+// ***************** Chapter No 3 NUMBER ****************
+
+
+
+// Qno 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+var age = 15;
+alert("i am " + age + " years old");
+
+
+var visited = 14;
+alert("you have visited this site " + visited + " times")
+
+
+// Qno 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthYear = 1999;
+document.write("my birth year is "+birthYear);
+
+
+// Qno 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+var clientName = "jhone Doe"
+var productTitle = "T-shirt"
+var quantity = 5;
+document.write(clientName + " ordered " + quantity + " " + productTitle + "(s) on XYZ clothing store")
+
+
+
+// ************ Chapter No 4 LEGAL & ILLEGAL  **************
+
+
+// Qno 1. Declare 3 variables in one statement.
+
+var one,two,three;
+
+// Qno 2. Declare 5 legal & 5 illegal variable names.
+
+// ---- legal variables ------
+
+var legal;
+var camelCase;
+var $variable;
+var _variable;
+var variable_1;
+
+// ------- illegal variables ---------
+
+// var 14214:
+// var Illegal;
+// var @Illegal;
+// var 4illegal;
+// var illegal variable;
+
+
+// Qno 3. Display this in your browser
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<h2>Legal variable names</h2>");
+document.write("<ul>");
+document.write("<li>var legal;</li>");
+document.write("<li>var camelCase;</li>");
+document.write("<li>var $variable;</li>");
+document.write("<li>var _variable;</li>");
+document.write("<li>var variable_1;</li>");
+document.write("</ul>");
+document.write("<h2>Illegal variable names</h2>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
